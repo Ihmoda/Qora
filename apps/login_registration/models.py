49c8584b03faf32915a158fe5c08c423a6271b27
=omar.ihmoda@gmail.com <=omar.ihmoda@gmail.com>
2017-11-21 14:48:05 -0800

    updated models file and included urls.py/views.py

--- apps/login_registration/models.py
+++ apps/login_registration/models.py
@@ -4,3 +4,55 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Topic(models.Model):
+    topic = models.CharField(max_length = 255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    users = models.ManyToManyField(User, related_name="interests")
+
+class Question(models.Model):
+    content = models.TextField()
+    anonymity = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Topic, related_name="questions")
+    user = models.ForeignKey(User, related_name = "questions")
+
+class Follow(models.Model):
+    rating = models.IntegerField()
+    following = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    follower = models.ForeignKey(User, related_name="questions_followed")
+    question = models.ForeignKey(Question, related_name="followers")
+
+class Answer(models.Model):
+    content = models.TextField()
+    published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    question = models.ForeignKey(Question, related_name="answers")
+
+class AnswerComment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    answer = models.ForeignKey(Answer, related_name="comments")
+
+class QuestionComment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    question = models.ForeignKey(Question, related_name="comments")
+
+
+
+
